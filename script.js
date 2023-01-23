12a31994be248f08e10d9fa273cb1a14227ae596
Your Full Name <ikopylevich@totalwine.com>
2023-01-23 14:03:48 -0500

    TT-18-Add Toggle Button

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 console.log("Engineering Training");
 
-var modalButton = document.getElementById('modalButton');
+const modalButton = document.getElementById('modalButton');
+const modalContainer = document.getElementById('modalContainer');
+
+modalButton.addEventListener("click", function () {
+    console.log("clicked button!");
+    const result = modalContainer.classList.toggle("hidden");
+});
 
 console.log("modalButton ", modalButton);
