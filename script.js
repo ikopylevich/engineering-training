5f0da623ed0e251a076addeb085eb1cf69a8ae26
Your Full Name <ikopylevich@totalwine.com>
2023-01-25 09:52:45 -0500

    TT-18-change

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const modalContainer = document.getElementById('modalContainer');
 
 modalButton.addEventListener("click", function () {
     console.log("clicked button!");
-    const result = modalContainer.classList.toggle("hidden");
+    modalContainer.classList.toggle("hidden");
 });
 
 console.log("modalButton ", modalButton);
