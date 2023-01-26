d06a1dcb5887a72c83cf8596f21ccdfd89849380
Your Full Name <ikopylevich@totalwine.com>
2023-01-26 11:45:05 -0500

    TT-19-avaScript: getElementsByClassName - close modal Part 1

--- script.js
+++ script.js
@@ -2,10 +2,14 @@ console.log("Engineering Training");
 
 const modalButton = document.getElementById('modalButton');
 const modalContainer = document.getElementById('modalContainer');
+const closeModalButton = document.getElementsByClassName("close-modal-button");
 
 modalButton.addEventListener("click", function () {
     console.log("clicked button!");
     modalContainer.classList.toggle("hidden");
 });
-
+closeModalButton[0].addEventListener("click", () => {
+    modalContainer.classList.toggle("hidden");
+});
 console.log("modalButton ", modalButton);
+console.log("closeModalButton : ", closeModalButton);
