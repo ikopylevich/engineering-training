87a1d9bb5a985d9c3f1e6caea7f0a07850016215
Your Full Name <ikopylevich@totalwine.com>
2023-08-21 14:19:07 -0400

    TT-136

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
 const express = require('express')
+const path = require('path');
 const app = express()
 const port = 3000
+const router = express.Router();
 
 // respond with "hello world" when a GET request is made to the homepage
-app.get('/', (req, res) => {
-    res.send('hello world')
-})
+router.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname + '/index.html'));
+});
+
+app.use('/', router);
 
 app.listen(port, () => {
-    console.log(`This app listening on port ${port}`)
-}) 
+    console.log(`This app listening on port ${port}`);
+}); 
